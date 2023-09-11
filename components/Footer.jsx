9de11edf5822daf00408f13eaf91d44055d650d7
MohammedJawad1920~ <ksdjabbu@gmail.com>
2023-09-11 13:57:44 +0530

    added footer

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="shadow-lg bg-prussianBlueDarkest w-full h-12 md:h-16">
+      <div className="container h-full mx-auto flex justify-center items-center">
+        <p className="text-white">&copy; 2023 Aniinfo App</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
